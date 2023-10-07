bd055a9d38852cfa294177c0fbd0716fa874a52d
kwalton95 <devkieran@proton.me>
2023-10-07 18:31:54 +0100

    deleted old files

--- .config/nvim/init.lua
+++
@@ -1,8 +0,0 @@
--- Handle plugins with lazy.nvim
-require("core.lazy")
-
--- General Neovim keymaps
-require("core.keymaps")
-
--- Other options
-require("core.options")
--- .config/nvim/lua/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
--- See current buffers at the top of the editor
-return {
-	{
-		"akinsho/bufferline.nvim",
-		version = "v3.*",
-		dependencies = "nvim-tree/nvim-web-devicons",
-		opts = {},
-	},
-}
